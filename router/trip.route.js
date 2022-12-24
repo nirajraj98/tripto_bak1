5478d941b78a1f825d9d45cb3036540bb357addd
Niraj Kumar <83273099+nirajraj98@users.noreply.github.com>
2022-12-24 11:46:01 +0530

    new

--- router/trip.route.js
+++ router/trip.route.js
@@ -5,6 +5,52 @@ const { TripModel } =require("../model/trip.model")
 const tripRoute=express.Router()
 
 
+tripRoute.get("/hotel",async (req, res) => {
+    try {
+        const page = parseInt(req.query.page);
+        const size = parseInt(req.query.size);
+        const sort= req.query.price
+        const skip = (page -1) * size;
+        
+        const total = await TripModel.find({category:"hotels"}).countDocuments();
+        let trip=await TripModel.find({category:"hotels"}).sort({price : sort}).skip(skip).limit(size);
+        
+        res.json({
+            records:trip,
+            total,
+            page, 
+            size
+        });
+    } catch(error) {
+        console.log(error)
+        res.status(400).json(error)
+    }
+})
+
+// tripRoute.get("/hotel",async(req,res)=>{
+//     const {limit=8,page=1}=req.query
+//     const total = await TripModel.find({category:"hotels"}).countDocuments();
+//     try{
+//       let trip=await TripModel.find({category:"hotels"})
+//       .limit(limit)
+//       .skip((page-1)*limit)
+     
+//       res.send({trip,total})
+//     }catch(err){
+//         res.status(400).send(e.message)
+//     }
+// })
+
+tripRoute.get("/hotel/:id",async(req,res)=>{
+
+    
+    try{
+       const trip = await TripModel.findById({_id: req.params.id})
+       res.status(200).json(trip)
+    }catch(err){
+        res.status(201).json(err)
+    }
+})
 tripRoute.get("/",async(req,res)=>{
     try{
        const trip = await TripModel.find({category:"Overview"})
@@ -33,15 +79,15 @@ tripRoute.get("/place",async(req,res)=>{
     }
 })
 
-tripRoute.get("/hotel",async(req,res)=>{
-    try{
-       const trip = await TripModel.find({category:"hotels"})
-       res.status(200).json(trip)
-    }catch(err){
-        res.status(201).json(err)
+// tripRoute.get("/hotel",async(req,res)=>{
+//     try{
+//        const trip = await TripModel.find({category:"hotels"})
+//        res.status(200).json(trip)
+//     }catch(err){
+//         res.status(201).json(err)
         
-    }
-})
+//     }
+// })
 
 tripRoute.get(":key",async(req,res)=>{
     let data= await TripModel.find({
@@ -51,13 +97,6 @@ tripRoute.get(":key",async(req,res)=>{
     
     })
 })
-
-
-
-
-
-
-
 
 
 tripRoute.post("/create",async(req,res)=>{
